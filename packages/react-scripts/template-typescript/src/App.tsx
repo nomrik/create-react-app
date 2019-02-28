695986e7c81adf210306b3e86a2c0316355215b0
Omri Kochavi <omri.kochavi@asperii.com>
2019-02-28 12:36:53 -0500

    update template files

--- packages/react-scripts/template-typescript/src/App.tsx
+++ packages/react-scripts/template-typescript/src/App.tsx
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <p>
             Import callRemote to call apex methods.
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Have a look in <code>README.md</code> for insturctions to hook up
+            the React project to Salesforce.
           </p>
           <p>
-            Import callRemote to call apex methods.
+            Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>Import callRemote to call apex methods.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
